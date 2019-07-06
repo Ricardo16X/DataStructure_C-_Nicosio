951a3ead596b798a8088308068d64454e9f9157e
Ricardo16X <ricardope1664@gmail.com>
2019-07-06 13:21:54 -0600

    Insertion Sort - Descendente

--- Ordenamiento/InsertionSort/Program.cs
+++ Ordenamiento/InsertionSort/Program.cs
@@ -12,25 +12,22 @@ namespace InsertionSort
         static void Main(string[] args)
         {
             Lista_Ligada lista = new Lista_Ligada();
-            lista.Agregar(81);
-            lista.Agregar(12);
-            lista.Agregar(65);
-            lista.Agregar(76);
-            lista.Agregar(12);
-            lista.Agregar(27);
-            lista.Agregar(78);
-            lista.Agregar(38);
-            lista.Agregar(58);
-            lista.Agregar(10);
+            lista.Agregar(5);
+            lista.Agregar(2);
+            lista.Agregar(4);
+            lista.Agregar(8);
             lista.Transversa();
-
+            Console.WriteLine(lista[1]);
+            Console.WriteLine(lista[2]);
+            Console.WriteLine(lista[3]);
+            Console.WriteLine(lista[4]);
             //Insertion Sort
             int cantidad = lista.getSize() + 1;
             int indexAgujero = 0;
             int dato = 0;
-
+            // Ascendente
             //Recorremos los elementos
-            for (int i = 1; i < cantidad; i++)
+            for (int i = 0; i < cantidad; i++)
             {
                 //Obtenemos el dato
                 dato = (int)lista[i];
@@ -44,7 +41,33 @@ namespace InsertionSort
                 }
                 //Le colocamos al agujero el dato correspondiente
                 lista[indexAgujero] = dato;
-                lista.Transversa();
+                //lista.Transversa();
+            }
+            //Mostrar lista ordenada
+            Console.WriteLine("Lista Ordenada");
+            lista.Transversa();
+
+            // Descendente
+            //Insertion Sort
+            int size = lista.getSize() + 1;
+            int indiceHole = 0;
+            int numerito = 0;
+            // Ascendente
+            //Recorremos los elementos
+            for (int i = 2; i < size; i++)
+            {
+                //Obtenemos el dato
+                numerito = (int)lista[i];
+                //Indicamos la posición del agujero
+                indiceHole = i;
+                //Recorremos los elementos hacia el agujero
+                while (indiceHole > 1 && lista[indiceHole - 1] < numerito)
+                {
+                    lista[indiceHole] = lista[indiceHole - 1];
+                    indiceHole -= 1;
+                }
+                //Le colocamos al agujero el dato correspondiente
+                lista[indiceHole] = numerito;
             }
             //Mostrar lista ordenada
             Console.WriteLine("Lista Ordenada");
